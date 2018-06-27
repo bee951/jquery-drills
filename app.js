dd0565bf66f88ef27c7cb359155c622ea5b49519
Ben Easterwood <ben.easterwood@gmail.com>
2018-06-26 23:10:02 -0500

    h2 generation added some issues

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
+    $('#btnSubmit').prop("disabled",true);
+    $("input[type=text]").change(function() {
+        let validate=true;
+        if($("input[type=text]").val().length===0)validate=false;
+        if(validate)$('#btnSubmit').removeAttr('disabled');})
+    $('input[type=text]:first').trigger('change');
     $('#btnSubmit').click(function() {
-        alert("The button has been clicked.")
+        alert($("input[type=text]").val())
+        $("div").append("<h2>"+$("input[type=text]").val()+"</h2>");
     })
+    $("body").append("<div></div>");
 })
