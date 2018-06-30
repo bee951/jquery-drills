944bf1f1a7ca432a060cf87322a59fd14d92c014
Ben Easterwood <ben.easterwood@gmail.com>
2018-06-29 23:23:58 -0500

    completed lab

--- app.js
+++ app.js
@@ -4,10 +4,32 @@ $(document).ready(function() {
         let validate=true;
         if($("input[type=text]").val().length===0)validate=false;
         if(validate)$('#btnSubmit').removeAttr('disabled');})
-    $('input[type=text]:first').trigger('change');
-    $('#btnSubmit').click(function() {
-        alert($("input[type=text]").val())
-        $("div").append("<h2>"+$("input[type=text]").val()+"</h2>");
-    })
     $("body").append("<div></div>");
+    $("div").append("<ul></ul>");
+    $('#btnSubmit').click(function() {
+        $("ul").append("<li>"+$("input[type=text]").val()+"</li>")
+        /*alert($("input[type=text]").val())
+        $("div").append("<h2>"+$("input[type=text]").val()+"</h2>");
+        $("h2").on("mouseover",function() {
+            $(this).css({
+                "background-color": rcolor(),
+                "border-radius": rborder()
+        })
+        })  */
+        $("li").click(function() {
+            $(this).css({"color": rcolor()})
+        })
+        $("li").dblclick(function() {
+            $(this).remove()
+        })
+    })
+     
+    function rcolor() {
+        var rc='rgb('+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+','+Math.floor(Math.random()*255)+')'
+        return rc
+    }
+    function rborder() {
+        var rb=Math.floor(Math.random()*10)+'px'
+        return rb
+    }
 })
